58b4dbc2f4a263922948559ec71d0d54889e7162
tragicmanner <dragonkensuke@gmail.com>
2018-10-29 15:18:26 -0600

    Refactor of coroutines and general code for 100% arduino

--- gbtrap/gbtrap.ino
+++ gbtrap/gbtrap.ino
@@ -7,32 +7,42 @@
 
 #include <Coroutines.h>
 #include <Servo.h>
+#include <SoftwareSerial.h>
+#include <YX5300.h>
+
+// --door variables: state--
+unsigned long doorTimer = 0;
+const int doorWaitTime = 300; // If your doors don't have enough time to close, make this longer
 
 // --trap variables
 int trapState = 0; // trap states are 0 for idle, 1 for pedal press waiting for doors open, 2 for doors open and audio/LED show active, 3 for doors open and audio/LED show done, 4 for doors open and waiting to close
 const int trapIdle = 0;
 const int trapOpening = 1;
 const int trapOpenShow = 2;
-const int trapOpenIdle = 3;
+const int trapCapturing = 3;
 const int trapClosing = 4;
+const int trapCheckingCapture = 5;
+const int trapCaptured = 6;
+
+int stateDurations[7] = {0, 1302, 11000, 6900, doorWaitTime, 1087, 5775}; // Each index corresponds with the states defined above. If the value is 0, then the state isn't governed by time
+bool pedalChangesState[7] = {true, false, true, false, false, false, false};
+
+int timeBetweenBeeps = 336; //time between beeps in audio file for after the graph is full and the trap starts beeping
+int numberOfBeeps = 15;
+int timeBeforeFirstBeep = 125;
+int redLightDurationForBeep = 58;
 
 // --door variables: servo1--  SERVO OPEN AND CLOSED VALUES NEED TO BE ADJUSTED FOR YOUR SERVOS!
 Servo servo1;
-const int servo1Pin = 9;
+const int servo1Pin = 3;
 const int servo1OpenPosition = 68;
-const int servo1ClosedPosition = 23;
+const int servo1ClosedPosition = 15;
 
 // --door variables: servo_2--
 Servo servo2;
 const int servo2Pin = 10;
 const int servo2OpenPosition = 15;
-const int servo2ClosedPosition = 63;
-
-// --door variables: state--
-unsigned long doorTimer = 0;
-const int doorWaitTime = 300; // If your doors don't have enough time to close, make this longer
-const unsigned long doorOpenTime = 15000; // This is how long the audio and LEDs are active after the doors are first opened. This time is also used to know when to auto-close doors.
-const bool autoCloseDoor = true; // If this is true, then after doorOpenTime has elapsed after the doors open, the doors will automatically close.
+const int servo2ClosedPosition = 68;
 
 // --Pedal variables--
 const int pedalPin = 2;
@@ -40,159 +50,346 @@ const int pedalPin = 2;
 int pedalState = 0;
 int latestPedal = 0;
 
-// -- General LED variables--
-int PWMLEDs[4] = {3,5,6,11}; // the PWM pins that you want to run your LEDs with
-int nonPWMLEDs[2] = {7,8}; // the pins that you want to run non-PWM LEDs with -- These are LEDs that just turn on and off
+// -- General LED variables-- (LED Pins)
+int PWMPink = 9;
+int redLED = 4;
+int whiteLED01 = 7;
+int whiteLED02 = 8;
+int PWMBlue = 11;
+int blueLED = 12;
+int pinkLED = 13;
+int barLED01 = 14;
+int barLED02 = 15;
+int barLED03 = 16;
+int yellowLED = 17;
+int whiteLED03 = 18;
+int whiteLED04 = 19;
+
+int currentLED = 0;
+int currentLEDTime = 0;
+int currentPauseAtHigh = 0;
+int currentPauseAtLow = 0;
+int currentStartingBrightness = 0;
+int currentBrightnessInterval = 0;
 
 // --PWM LED Variables--
-int pwmTime = 12000; // How long in ms the PWM LEDs will flash for until they are turned off
-int brightness[4] = {0, 255, 100, 255}; // The starting brightness for each PWM pin. 0 is off, 255 is full brightness
-int fadeAmount[4] = {100,50,50,50}; // The higher this amount, the faster the LED will fade between on and off
 int fadeStep = 30; // This controls how smooth the LED fades will be
+int PWMPins[2] = {PWMBlue, PWMPink};
+int PWMPauseHighOpen[2] = {50, 50};
+int PWMPauseLowOpen[2] = {50, 50};
+int PWMFadeAmountOpen[2] = {30, 30};
+int PWMPauseHighCapture[2] = {25, 25};
+int PWMPauseLowCapture[2] = {25, 25};
+int PWMFadeAmountCapture[2] = {50, 50};
+int PWMStartingBrightnessOpen[2] = {0, 100};
+int PWMStartingBrightnessCapture[2] = {100, 0};
 
+// --PWM LED Variables that shouldn't be touched--
+int PWMCurrentBrightness[2] = {0, 0};
+int PWMCurrentPauseHigh[2] = {0, 0};
+int PWMCurrentPauseLow[2] = {0, 0};
+int PWMCurrentFadeAmount[2] = {0, 0};
 
 // -- Coroutine Stuff --
-Coroutines<3> coroutines;
+Coroutines<7> coroutines;
+
+// -- Audio Chip Stuff --
+int RX_Pin = 5; //The pin the RX connection hooks into
+int TX_Pin = 6; //The pin the TX connection hooks into
+YX5300 audio; //Setting up the audio chip RX on pin 5, and TX on pin 6
 
 
-void setup() {
+void setup() 
+{
   //Serial.begin(9600);
+  // Initialize LEDs
+  InitializeLEDs();
+  // Turn on LEDs that need to start on
+  digitalWrite(redLED, HIGH);
   // Set up the pin that watches for the pedal button
   pinMode(pedalPin, INPUT_PULLUP);
-  for(int i = 0; i < sizeof(PWMLEDs)/sizeof(int); i++){
-    pinMode(PWMLEDs[i], OUTPUT);
-  }
-  for(int k = 0; k < sizeof(nonPWMLEDs)/sizeof(int); k++){
-    pinMode(nonPWMLEDs[k], OUTPUT);
-  }
-  coroutines.start(CloseDoors);
+  pedalState = digitalRead(pedalPin);
+  // Make sure servos are closed
+  AttachServos();
+  MoveServos(servo1ClosedPosition, servo2ClosedPosition);
+  delay(doorWaitTime);
+  latestPedal = pedalState;
+  // Wait for the audio player to start up, then initialize it
+  delay(500);
+  audio.Initialize();
+  DetachServos();
+  delay(200); //wait for the audio player to be ready 
+  audio.PlayFileWithVolume(8, 30);
+  delay(2448);
+  audio.SetVolume(30);
+  delay(20);
+  audio.CycleAudio(9);
 }
 
 void loop() {
   // First, read the state of the pedal
   pedalState = digitalRead(pedalPin);
 
-  bool startPedalStuff = false;
+  bool changeState = false;
 
-  if(latestPedal != pedalState){
+  if(latestPedal != pedalState)
+  {
     latestPedal = pedalState;
+    if(pedalChangesState[trapState])
+    {
+      changeState = true;
+    }
+  }
 
-    startPedalStuff = true;
+  // Check if it's time to change state
+  if(stateDurations[trapState] > 0 && millis() >= doorTimer)
+  {
+    changeState = true;
   }
 
   // Update the LEDs and coroutines
   coroutines.update();
 
-  // This block detects if a button has been newly pressed (button down) and the trap is in a state to accept pedal presses
-  if (startPedalStuff && trapState != trapOpening && trapState != trapClosing) {
+  if(changeState)
+  {
+    trapState ++;
+    Serial.print(trapState + "\n");
+    if(trapState > trapCaptured)
+    {
+      trapState = 0;
+    }
+    doorTimer = millis() + stateDurations[trapState];
 
-    if (trapState == trapIdle) {
-      doorTimer = millis() + doorOpenTime;
-      coroutines.start(OpenDoors);
-    }
-    else if ((trapState == trapOpenShow || trapState == trapOpenIdle )) {
-      coroutines.start(CloseDoors);
-    }
-  }
-  // This checks if the doors have been open for a certain amount of time
-  else if(trapState == trapOpenShow && millis() >= doorTimer) {
-    trapState = trapOpenIdle;
-    if(autoCloseDoor){
-      coroutines.start(CloseDoors);
+    switch(trapState)
+    {
+      case trapIdle:
+        TrapIdle();
+        break;
+      case trapOpening:
+        Serial.print("OpeningDoors\n");
+        coroutines.start(OpenDoors);
+        break;
+      case trapOpenShow:
+        Serial.print("OpenShow\n");
+        OpenShow();
+        break;
+      case trapCapturing:
+        Serial.print("CapturingGhost\n");
+        Capturing();
+        break;
+      case trapClosing:
+        Serial.print("ClosingTrap\n");
+        coroutines.start(CloseDoors);
+        break;
+      case trapCheckingCapture:
+        Serial.print("CheckingCapacityAndCapture\n");
+        coroutines.start(CheckCapture);
+        break;
+      case trapCaptured:
+        Serial.print("GhostCaptured\n");
+        coroutines.start(TrapCaptured);
+        break;
     }
   }
 }
 
-void OpenDoors(COROUTINE_CONTEXT(coroutine)) {
+void TrapIdle()
+{
+  audio.SetVolume(30);
+  delay(20);
+  audio.CycleAudio(9);
+  TurnOffAllLEDs();
+  digitalWrite(redLED, HIGH);
+}
+
+void OpenDoors(COROUTINE_CONTEXT(coroutine)) 
+{
   BEGIN_COROUTINE;
-  trapState = trapOpening;
+  audio.PlayFileWithVolume(1, 30);
   AttachServos();
   MoveServos(servo1OpenPosition, servo2OpenPosition);
+  digitalWrite(whiteLED01, HIGH);
+  digitalWrite(whiteLED02, HIGH);
+  digitalWrite(whiteLED03, HIGH);
+  digitalWrite(whiteLED04, HIGH);
+  digitalWrite(pinkLED, HIGH);
+  digitalWrite(blueLED, HIGH);
   coroutine.wait(doorWaitTime);
   COROUTINE_YIELD;
-  TurnOnNonPWMLEDs();
   DetachServos();
-  trapState = trapOpenShow;
-  coroutines.start(FadeLED);
-  END_COROUTINE;
-}
-
-void CloseDoors(COROUTINE_CONTEXT(coroutine)) {
-  BEGIN_COROUTINE;
-  trapState = trapClosing;
-  AttachServos();
-  MoveServos(servo1ClosedPosition, servo2ClosedPosition);
-  coroutine.wait(doorWaitTime);
-  COROUTINE_YIELD;
-  TurnOffNonPWMLEDs();
-  DetachServos();
-  trapState = trapIdle;
   //coroutines.start(FadeLED);
   END_COROUTINE;
 }
 
+void CloseDoors(COROUTINE_CONTEXT(coroutine)) 
+{
+  BEGIN_COROUTINE;
+  AttachServos();
+  MoveServos(servo1ClosedPosition, servo2ClosedPosition);
+  coroutine.wait(doorWaitTime);
+  COROUTINE_YIELD;
+  DetachServos();
+  //coroutines.start(FadeLED);
+  END_COROUTINE;
+}
+
+void OpenShow()
+{
+  audio.PlayFileWithVolume(5, 30);
+  digitalWrite(whiteLED01, HIGH);
+  digitalWrite(whiteLED02, HIGH);
+  digitalWrite(whiteLED03, HIGH);
+  digitalWrite(whiteLED04, HIGH);
+  digitalWrite(pinkLED, HIGH);
+  digitalWrite(blueLED, HIGH);
+  coroutines.start(FadeLED);
+}
+
+void Capturing()
+{
+  audio.PlayFileWithVolume(3, 30);
+  digitalWrite(whiteLED01, HIGH);
+  digitalWrite(whiteLED02, HIGH);
+  digitalWrite(whiteLED03, HIGH);
+  digitalWrite(whiteLED04, HIGH);
+  digitalWrite(pinkLED, HIGH);
+  digitalWrite(blueLED, HIGH);
+  coroutines.start(FadeLED);
+}
+
+void CheckCapture(COROUTINE_CONTEXT(coroutine))
+{
+  BEGIN_COROUTINE;
+  TurnOffAllLEDs();
+  digitalWrite(redLED, HIGH);
+  audio.PlayFileWithVolume(2, 30);
+  digitalWrite(barLED01, HIGH);
+  coroutine.wait(156);
+  COROUTINE_YIELD;
+  digitalWrite(barLED02, HIGH);
+  coroutine.wait(156);
+  COROUTINE_YIELD;
+  digitalWrite(barLED03, HIGH);
+  coroutine.wait(156);
+  COROUTINE_YIELD;
+  digitalWrite(yellowLED, HIGH);
+  coroutine.wait(156);
+  COROUTINE_YIELD;
+  //digitalWrite(barLED01, LOW);
+  //digitalWrite(barLED02, LOW);
+  //digitalWrite(barLED03, LOW);
+  END_COROUTINE;
+}
+
+void TrapCaptured(COROUTINE_CONTEXT(coroutine))
+{
+  COROUTINE_LOCAL(int, i);
+  BEGIN_COROUTINE;
+  audio.PlayFileWithVolume(4, 30);
+  coroutine.wait(timeBeforeFirstBeep);
+  COROUTINE_YIELD;
+  for(i=0; i < numberOfBeeps; i++)
+  {
+    digitalWrite(redLED, HIGH);
+    coroutine.wait(redLightDurationForBeep);
+    COROUTINE_YIELD;
+    digitalWrite(redLED, LOW);
+    coroutine.wait(timeBetweenBeeps);
+    COROUTINE_YIELD;
+  }
+  END_COROUTINE;
+}
+
 void FadeLED(COROUTINE_CONTEXT(coroutine)) {
-  COROUTINE_LOCAL(unsigned long, finishTime);
+  COROUTINE_LOCAL(int, local_state);
+  COROUTINE_LOCAL(int, i);
   
   BEGIN_COROUTINE;
+  
+  local_state = trapState;
 
-  finishTime = millis() + pwmTime;
+  if(trapState == trapOpenShow)
+  {
+    copy(PWMStartingBrightnessOpen, PWMCurrentBrightness, 4);
+    copy(PWMPauseHighOpen, PWMCurrentPauseHigh, 4);
+    copy(PWMPauseLowOpen, PWMCurrentPauseLow, 4);
+    copy(PWMFadeAmountOpen, PWMCurrentFadeAmount, 4);
+  }
+
+  if(trapState == trapCapturing)
+  {
+    copy(PWMStartingBrightnessCapture, PWMCurrentBrightness, 4);
+    copy(PWMPauseLowCapture, PWMCurrentPauseHigh, 4);
+    copy(PWMPauseLowCapture, PWMCurrentPauseLow, 4);
+    copy(PWMFadeAmountCapture, PWMCurrentFadeAmount, 4);
+  }
+
+  coroutine.wait(fadeStep);
+  COROUTINE_YIELD;
   
-  while(finishTime > millis() && trapState == trapOpenShow) {
-    for (int i = 0; i < sizeof(PWMLEDs)/sizeof(int); i++)
+  while(trapState == local_state)
+  {
+    for(i = 0; i < (sizeof(PWMPins)/sizeof(int)); i++)
     {
-      analogWrite(PWMLEDs[i], brightness[i]);
-      
+      //Serial.print("Pin: " + String(i) + " Bright: " + String(PWMCurrentBrightness[i]) + "Fade: " + String(PWMCurrentFadeAmount[i]) + "\n");
       // change the brightness for next time through the loop:
-      brightness[i] = brightness[i] + fadeAmount[i];
-  
-      if (brightness[i] > 255){
-        brightness[i] = 255;
+      PWMCurrentBrightness[i] = PWMCurrentBrightness[i] + PWMCurrentFadeAmount[i];
+    
+      if (PWMCurrentBrightness[i] > 255)
+      {
+        PWMCurrentBrightness[i] = 255;
       }
-  
-      if (brightness[i] < 0) {
-        brightness[i] = 0;
+      else if (PWMCurrentBrightness[i] < 0) 
+      {
+        PWMCurrentBrightness[i] = 0;
       }
     
       // reverse the direction of the fading at the ends of the fade:
-      if (brightness[i] <= 0 || brightness[i] >= 255) {
-        fadeAmount[i] = -fadeAmount[i];
+      if (PWMCurrentBrightness[i] <= 0 || PWMCurrentBrightness[i] >= 255) 
+      {
+        PWMCurrentBrightness[i] = -PWMCurrentBrightness[i];
       }
+
+      analogWrite(PWMPins[i], PWMCurrentBrightness[i]);
     }
     coroutine.wait(fadeStep);
     COROUTINE_YIELD;
   }
-
-  TurnOffPWMLEDs();
   END_COROUTINE;
 }
 
-void TurnOnPWMLEDs(){
-  for(int i = 0; i < sizeof(PWMLEDs)/sizeof(int); i++)
-  {
-    analogWrite(PWMLEDs[i], 255);
-  }
+void InitializeLEDs()
+{
+  pinMode(PWMPink, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(whiteLED01, OUTPUT);
+  pinMode(whiteLED02, OUTPUT);
+  pinMode(PWMBlue, OUTPUT);
+  pinMode(blueLED, OUTPUT);
+  pinMode(pinkLED, OUTPUT);
+  pinMode(barLED01, OUTPUT);
+  pinMode(barLED02, OUTPUT);
+  pinMode(barLED03, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(whiteLED03, OUTPUT);
+  pinMode(whiteLED04, OUTPUT);
 }
 
-void TurnOffPWMLEDs(){
-  for(int i = 0; i < sizeof(PWMLEDs)/sizeof(int); i++)
-  {
-    analogWrite(PWMLEDs[i], 0);
-  }
-}
-
-void TurnOnNonPWMLEDs(){
-  for(int i = 0; i < sizeof(nonPWMLEDs)/sizeof(int); i++)
-  {
-    digitalWrite(nonPWMLEDs[i], HIGH);
-  }
-}
-
-void TurnOffNonPWMLEDs(){
-  for(int i = 0; i < sizeof(nonPWMLEDs)/sizeof(int); i++)
-  {
-    digitalWrite(nonPWMLEDs[i], LOW);
-  }
+void TurnOffAllLEDs()
+{
+  digitalWrite(PWMPink, LOW);
+  digitalWrite(redLED, LOW);
+  digitalWrite(whiteLED01, LOW);
+  digitalWrite(whiteLED02, LOW);
+  digitalWrite(PWMBlue, LOW);
+  digitalWrite(blueLED, LOW);
+  digitalWrite(pinkLED, LOW);
+  digitalWrite(barLED01, LOW);
+  digitalWrite(barLED02, LOW);
+  digitalWrite(barLED03, LOW);
+  digitalWrite(yellowLED, LOW);
+  digitalWrite(whiteLED03, LOW);
+  digitalWrite(whiteLED04, LOW);
 }
 
 void AttachServos() {
@@ -208,4 +405,8 @@ void MoveServos(int servo1Position, int servo2Position) {
 void DetachServos() {
   servo1.detach();
   servo2.detach();
+}
+
+void copy(int* src, int* dst, int len) {
+    memcpy(dst, src, sizeof(src[0])*len);
 }
